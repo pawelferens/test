3be13a77faca70d0077eb30fedae654f668b7f23
Paweł <pawel@velia-systems.com>
2013-02-14 14:51:08 +0100

    Initial Commit

---
+++ BridgeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BridgeTest
+//
+//  Created by Pawel Ferens on 2/14/13.
+//  Copyright (c) 2013 Pawel Ferens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BridgeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BridgeTest
+//
+//  Created by Pawel Ferens on 2/14/13.
+//  Copyright (c) 2013 Pawel Ferens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
